c611af5d5a657e3238bbd8d1dde712e65abe1141
Fendy <zf@fendy5.cn>
2023-03-02 01:08:52 +0800

    refactor: 重构TodoList组件

--- src/utils/echo.ts
+++ src/utils/echo.ts
@@ -4,11 +4,13 @@ import { UserModule } from '@/store/modules/user'
 export const echo = new Echo({
   broadcaster: 'pusher',
   key: 'ws_todo',
-  auth: { headers: { 'Authorization': 'Bearer ' + UserModule.token, Accept: 'application/json, text/plain, */*' } },
+  auth: { headers: { 'Authorization': 'Bearer ' + UserModule.token, Accept: 'application/json' } },
   cluster: 'mt1',
-  authEndpoint: `https://todo.fendy5.cn/api/v1/broadcasting/auth`,
-  // forceTLS: false,
-  // wsPort: 6001,
+  authEndpoint: `${process.env.VUE_APP_BASE_API}/api/v1/broadcasting/auth`,
+  // authEndpoint: `https://todo.fendy5.cn/api/v1/broadcasting/auth`,
+  forceTLS: false,
+  wsPort: 6001,
+  // wsHost: '127.0.0.1',
   wsHost: 'todo.fendy5.cn',
   encrypted: true,
   enabledTransports: ['ws', 'wss']
