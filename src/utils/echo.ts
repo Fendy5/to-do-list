0aab61a6fbcb31d6f065af276b0037e31c3ddf91
Fendy <zf@fendy5.cn>
2023-03-01 20:54:14 +0800

    perf: ws配置

--- src/utils/echo.ts
+++ src/utils/echo.ts
@@ -6,7 +6,7 @@ export const echo = new Echo({
   key: 'ws_todo',
   auth: { headers: { 'Authorization': 'Bearer ' + UserModule.token, Accept: 'application/json' } },
   cluster: 'mt1',
-  authEndpoint: '/api/v1/broadcasting/auth',
+  authEndpoint: '/web/broadcasting/auth',
   forceTLS: false,
   // wsPort: 6001,
   wsHost: 'todo.fendy5.cn',
