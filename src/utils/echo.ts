4e5f1bdfeb79c308cbdb55ab006ccea9968b0a89
Fendy <zf@fendy5.cn>
2023-03-01 20:47:26 +0800

    refactor: 重构TodoList组件

--- src/utils/echo.ts
+++ src/utils/echo.ts
@@ -6,7 +6,7 @@ export const echo = new Echo({
   key: 'ws_todo',
   auth: { headers: { 'Authorization': 'Bearer ' + UserModule.token, Accept: 'application/json' } },
   cluster: 'mt1',
-  authEndpoint: '/api/broadcasting/auth',
+  authEndpoint: '/api/v1/broadcasting/auth',
   forceTLS: false,
   // wsPort: 6001,
   wsHost: 'todo.fendy5.cn',
